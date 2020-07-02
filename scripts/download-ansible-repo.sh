451d1f88425add29bfa8414847873a201665ab47
Michalis Michaloliakos <michalis@hashicorp.com>
2020-07-02 21:48:13 +0100

    CICD-17: Debug: Checking curl/unzip on cloud_vm

--- scripts/download-ansible-repo.sh
+++ scripts/download-ansible-repo.sh
@@ -7,16 +7,17 @@ set -e -x
 
 echo 'Installing Ansible and git on host'
 # sudo apt-get update -y
-sudo apt-get install -y software-properties-common
-sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt-get install -y ansible git
+# sudo apt-get install -y software-properties-common
+# sudo apt-add-repository --yes --update ppa:ansible/ansible
+# sudo apt-get install -y ansible git
 
-ansible --version
-git version
+# ansible --version
+# git version
 
 cd /tmp/
-git clone https://github.com/michnmi/ansible_internal
+# git clone https://github.com/michnmi/ansible_internal
+curl -L https://github.com/michnmi/ansible_internal/archive/master.zip --output /tmp/master.zip
+unzip master.zip
+# cd ansible_internal
 
-cd ansible_internal
-
-ansible-playbook  -i inventories/cloud_vms/hosts.ini -l cloud_vm playbooks/cloud_vm.yml
+# ansible-playbook  -i inventories/cloud_vms/hosts.ini -l cloud_vm playbooks/cloud_vm.yml
