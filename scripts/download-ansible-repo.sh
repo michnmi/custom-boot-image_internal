0f3a9d79ef3c213e75784b06c9b3c081fc7f5589
Michalis Michaloliakos <michalis@hashicorp.com>
2020-07-02 23:39:02 +0100

    CICD-17: Should be last change. Removing git from packages

--- scripts/download-ansible-repo.sh
+++ scripts/download-ansible-repo.sh
@@ -5,20 +5,16 @@ set -e -x
 
 # Following the official ansible docs: https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html#installing-ansible-on-ubuntu
 
-echo 'Installing Ansible and git on host'
-# sudo apt-get update -y
+echo 'Installing Ansible'
 sudo apt-get install -y software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt-get install -y unzip
-sudo apt-get install -y ansible
-
+sudo apt-get install -y unzip ansible
 
 ansible --version
 
-cd /tmp/
-# git clone https://github.com/michnmi/ansible_internal
+echo 'Download and setup repo for ansible-playbook'
 curl -L https://github.com/michnmi/ansible_internal/archive/master.zip --output /tmp/master.zip
 unzip master.zip
-cd ansible_internal-master
+cd /tmp/ansible_internal-master
 
 ansible-playbook  -i inventories/cloud_vms/hosts.ini -l cloud_vm playbooks/cloud_vm.yml
