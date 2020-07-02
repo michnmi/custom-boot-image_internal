200f1fb3593ba1c712faddd2079a9653a644ed74
Michalis Michaloliakos <michalis@hashicorp.com>
2020-07-02 22:25:10 +0100

    CICD-17: Debug: try pip install ansible

--- scripts/download-ansible-repo.sh
+++ scripts/download-ansible-repo.sh
@@ -7,9 +7,13 @@ set -e -x
 
 echo 'Installing Ansible and git on host'
 # sudo apt-get update -y
-sudo apt-get install -y software-properties-common
-sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt-get install -y ansible unzip
+# sudo apt-get install -y software-properties-common
+# sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt-get install -y unzip
+
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py --user
+pip install --user ansible
 
 ansible --version
 
@@ -17,6 +21,6 @@ cd /tmp/
 # git clone https://github.com/michnmi/ansible_internal
 curl -L https://github.com/michnmi/ansible_internal/archive/master.zip --output /tmp/master.zip
 unzip master.zip
-cd ansible_internal
+cd ansible_internal-master
 
 ansible-playbook  -i inventories/cloud_vms/hosts.ini -l cloud_vm playbooks/cloud_vm.yml
