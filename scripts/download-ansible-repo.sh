27fce98787c7c9650d24f425b00ac2d6e244f061
Michalis Michaloliakos <michalis.michaloliakos@gmail.com>
2020-07-03 00:13:07 +0100

    CICD-17: Use curl / unzip and pip in the cloud-vm provisioning steps (#2)
    * CICD-17: Debug: Checking curl/unzip on cloud_vm
    
    * CICD-17: Debug: Setup ansible through pip
    
    * CICD-17: Debug: Package name issue
    
    * CICD-17: Debug: Keep ansible pkg
    
    * CICD-17: Debug: try pip install ansible
    
    * CICD-17: Try with python3
    
    * CICD-17: Debug: Install asible and unzip. Just.
    
    * CICD-17: Debug: Forgot the ansible repo
    
    * CICD-17: Debug: Let's see if we can enforce an apt cache
    
    * CICD-17: Should be last change. Removing git from packages
    
    * CICD-17: Debug: A small typo

--- scripts/download-ansible-repo.sh
+++ scripts/download-ansible-repo.sh
@@ -5,18 +5,18 @@ set -e -x
 
 # Following the official ansible docs: https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html#installing-ansible-on-ubuntu
 
-echo 'Installing Ansible and git on host'
-# sudo apt-get update -y
+echo 'Installing Ansible'
 sudo apt-get install -y software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt-get install -y ansible git
+sudo apt-get install -y unzip ansible
 
 ansible --version
-git version
 
-cd /tmp/
-git clone https://github.com/michnmi/ansible_internal
+echo 'Download and setup repo for ansible-playbook'
+curl -L https://github.com/michnmi/ansible_internal/archive/master.zip --output /tmp/master.zip
 
-cd ansible_internal
+cd /tmp
+unzip master.zip
+cd ansible_internal-master
 
 ansible-playbook  -i inventories/cloud_vms/hosts.ini -l cloud_vm playbooks/cloud_vm.yml
