e06a75018b6126ec0fc0494e66144b7eacd6a089
Michalis Michaloliakos <michail.michaloliakos@docker.com>
2024-03-07 17:11:12 +0000

    CICD-76: Let's remove snapd and lxd because they annoy me

--- scripts/clean-up.sh
+++ scripts/clean-up.sh
@@ -10,4 +10,18 @@ rm /home/packer/.ssh/authorized_keys
 
 echo "Remove ansible"
 sudo apt-get remove -y ansible
+
+echo "Remove snapd"
+sudo systemctl disable snapd.service
+sudo systemctl disable snapd.socket
+sudo systemctl disable snapd.seeded.service
+sudo snap remove lxd
+sudo snap remove core20
+sudo snap remove snapd
+
+sudo rm -rf /var/cache/snapd/
+sudo rm -rf /root/snap
+sudo apt autoremove -y --purge snapd
+
+echo "Remove anything left"
 sudo apt autoremove -y
